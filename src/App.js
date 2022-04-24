a425de74c948b28c44f11d6066fd4d21a9b5a152
zorro200 <loren2018loren@gmail.com>
2022-04-24 21:22:53 +0200

    Add login Route Link and not show Link when logged

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import NotesContainer from './components/NotesContainer.jsx';
 import { initNotes } from './reducers/noteReducer.js';
 import './App.css';
 import { NoteDetail } from './components/NoteDetail.jsx';
+import Login from './components/Login.jsx'
 
 const App = () => {
   // Will allow us to use imported actions (DISPATCH FUNCTIONS)
-  const dispatch = useDispatch();
-  const notes = useSelector(state => state.notes);
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state.notes)
+  const user = useSelector((state) => state.users[0])
 
   // Each time the component have been mounted, and dispatch has been updated,
   // will show all the notes that we have on server side
@@ -22,11 +24,17 @@ const App = () => {
     <BrowserRouter>
       <header>
         <Link to='/' className='App-link'> Home </Link>
+        {
+          user
+            ? <em>Logged as {user.nick}</em>
+            : <Link to='/login' className='App-link'> Login </Link>
+        }
         <Link to='/notes' className='App-link'> Notes </Link>
         <Link to='/filters' className='App-link'> Filters </Link>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/notes/:id' element={<NoteDetail notes={notes} />} />
         <Route path='/notes' element={<NotesContainer />} />
 
